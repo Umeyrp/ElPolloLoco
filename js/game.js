458f9eb7c1308bf58c5b60c91f119645958de91b
Umeyrp <umeyr.kemiksiz@gmail.com>
2026-06-29 11:15:34 +0200

    Add Keyboard class and integrate into game logic

--- js/game.js
+++ js/game.js
@@ -1,8 +1,14 @@
 let canvas;
 let world;
+let keyboard = new Keyboard();
 
 function init() {
     canvas = document.getElementById('canvas');
     world = new World(canvas);
     ctx = canvas.getContext('2d');
 }
+
+window.addEventListener('keypress', (event) => {
+    console.log(event);
+    
+})
