097c4ebe2379c069f4f746c8663a4e13fa1add38
Umeyrp <umeyr.kemiksiz@gmail.com>
2026-06-29 11:00:56 +0200

    Enhance game classes: add animation and image loading for Character, Chicken, and Cloud; refactor BackgroundObject and MovableObject constructors.

--- classes/movable-object.class.js
+++ classes/movable-object.class.js
@@ -2,19 +2,33 @@ class MovableObject {
     x = 120;
     y = 290;
     img;
-    height= 150;
+    height = 150;
     width = 100;
+    imageCache = {};
+    currentImage = 0;
+    speed = 0.15;
 
-    loadImage(path){
+    loadImage(path) {
         this.img = new Image(); //ersetzt document.getElementById(img)
         this.img.src = path;
+    }
+
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+        });
     }
 
     moveRight() {
         console.log("Move right");
     }
 
-    moveLeft() {
 
+    moveLeft() {
+        setInterval(() => {
+            this.x -= this.speed;
+        }, 1000 / 60);
     }
 }
