a7c66e009b8d799f54b06b550b626295539a242d
Umeyrp <umeyr.kemiksiz@gmail.com>
2026-06-30 13:39:01 +0200

    Implement Level class and integrate game objects; refactor character and chicken animations; added endboss;

---
+++ classes/endboss.class.js
@@ -0,0 +1,24 @@
+class Endboss extends MovableObject {
+    height = 500;
+    width = 300;
+    y = -20;
+    IMAGES_WALKING = [
+        "img/4_enemie_boss_chicken/1_walk/G1.png",
+        "img/4_enemie_boss_chicken/1_walk/G2.png",
+        "img/4_enemie_boss_chicken/1_walk/G3.png",
+        "img/4_enemie_boss_chicken/1_walk/G4.png",
+    ];
+
+    constructor() {
+        super().loadImage(this.IMAGES_WALKING[0]);
+        this.loadImages(this.IMAGES_WALKING);
+        this.x = 1800;
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            this.playAnimation(this.IMAGES_WALKING);
+        }, 500);
+    }
+}
--- classes/world.class.js
+++ classes/world.class.js
@@ -1,27 +1,6 @@
 class World {
     character = new Character();
-    enemies = [
-        new Chicken(),
-        new Chicken(),
-        new Chicken()
-    ];
-    clouds = [
-        new Cloud()
-    ];
-    backgroundObject = [
-        new BackgroundObject("img/5_background/layers/air.png", 0),
-        new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 0),
-        new BackgroundObject("img/5_background/layers/2_second_layer/1.png", 0),
-        new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 0),
-        new BackgroundObject("img/5_background/layers/air.png", 720),
-        new BackgroundObject("img/5_background/layers/3_third_layer/2.png", 720),
-        new BackgroundObject("img/5_background/layers/2_second_layer/2.png", 720),
-        new BackgroundObject("img/5_background/layers/1_first_layer/2.png", 720),
-        new BackgroundObject("img/5_background/layers/air.png", 720 * 2),
-        new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 720 * 2),
-        new BackgroundObject("img/5_background/layers/2_second_layer/1.png", 720 * 2),
-        new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 720 * 2)
-    ];
+    level = level1;    
     canvas;
     ctx;
     keyboard;
@@ -30,7 +9,7 @@ class World {
     constructor(canvas, keyboard) {
         this.ctx = canvas.getContext('2d');
         this.canvas = canvas;
-        this.keyboard = keyboard;
+        this.keyboard = keyboard;        
         this.draw();
         this.setWorld();
     }
@@ -62,11 +41,11 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.ctx.translate(this.camera_x, 0);
-        this.addObjectsToMap(this.backgroundObject);
-        this.addObjectsToMap(this.clouds);
+        this.addObjectsToMap(this.level.backgroundObjects);
+        this.addObjectsToMap(this.level.clouds);
 
         this.addToMap(this.character);
-        this.addObjectsToMap(this.enemies);
+        this.addObjectsToMap(this.level.enemies);
         this.ctx.translate(-this.camera_x, 0);
         let self = this;
         requestAnimationFrame(function () {
